e94344be1903fbbf9c3b3771357d5c2fb5887c86
Jake Clearwater <jakeclearwater7@gmail.com>
2021-05-19 16:25:34 +1200

    creating my file

---
+++ Jake_studio_game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jake_studio_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
