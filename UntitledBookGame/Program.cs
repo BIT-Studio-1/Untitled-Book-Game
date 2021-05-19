0f5d445676a87ec8049d542231a15db83e87a64c
aardhynlavender <LAVEAT1@student.op.ac.nz>
2021-05-19 16:09:49 +1200

    Initalized Visual Studio project with .gitignore

---
+++ UntitledBookGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UntitledBookGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
