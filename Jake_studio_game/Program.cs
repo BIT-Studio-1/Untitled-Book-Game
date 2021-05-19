f6cbe7c8fcc6c0c8f039e57022029798bd7a3754
Jake Clearwater <jakeclearwater7@gmail.com>
2021-05-19 18:34:32 +1200

    created title screen

--- Jake_studio_game/Program.cs
+++ Jake_studio_game/Program.cs
@@ -6,7 +6,52 @@ namespace Jake_studio_game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("");
+            Console.WriteLine("      __________________________________________________");
+            Console.WriteLine("     |                       __   _   __   __           |");
+            Console.WriteLine("     |          |\\  /| |  | |__| | \\ |__  |__|          |");
+            Console.WriteLine("     |          | \\/ | |__| |  \\ |_/ |__  |  \\          |");
+            Console.WriteLine("     |                                                  |");
+            Console.WriteLine("     |                                                  |");
+            Console.WriteLine("     |               Press enter to begin               |");
+            Console.WriteLine("     |                                                  |");
+            Console.WriteLine("     |                                                  |");
+            Console.WriteLine("     |                                                  |");
+            Console.WriteLine("     |                       |\\                         |");
+            Console.WriteLine("     |                       | \\                        |");
+            Console.WriteLine("     |                       | |                        |");
+            Console.WriteLine("     |                       | |                        |");
+            Console.WriteLine("     |                       ||                         |");
+            Console.WriteLine("     |                       ||                         |");
+            Console.WriteLine("     |__________________________________________________|");
+            Console.WriteLine(" ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("");
+            Console.WriteLine("      __________________________________________________");
+            Console.WriteLine("     |                       __   _   __   __           |");
+            Console.WriteLine("     |          |\\  /| |  | |__| | \\ |__  |__|          |");
+            Console.WriteLine("     |          | \\/ | |__| |  \\ |_/ |__  |  \\          |");
+            Console.WriteLine("     |                                                  |");
+            Console.WriteLine("     |                                                  |");
+            Console.WriteLine("     |             Would you like to play?              |");
+            Console.WriteLine("     |                                                  |");
+            Console.WriteLine("     |                 Y     or      N                  |");
+            Console.WriteLine("     |                                                  |");
+            Console.WriteLine("     |                       |\\                         |");
+            Console.WriteLine("     |                       | \\                        |");
+            Console.WriteLine("     |                       | |                        |");
+            Console.WriteLine("     |                       | |                        |");
+            Console.WriteLine("     |                       ||                         |");
+            Console.WriteLine("     |                       ||                         |");
+            Console.WriteLine("     |__________________________________________________|");
+            
+            Console.Write(" >");
+            Console.ReadLine();
+
         }
     }
 }
