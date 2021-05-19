7a96c841c28b930b7dbb6bae323d4c97ac65cfea
aardhynlavender <laveat1@student.op.ac.nz>
2021-05-19 22:15:10 +1200

    Added Item class, defining basic methods and properties

--- AdventureBook/GameObjects/Item.cs
+++ AdventureBook/GameObjects/Item.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 namespace AdventureBook.GameObjects
 {
-    public class Item
+    public class Item : Sprite
     {
-        public Item()
+        // MEMBERS /////////////////////////////////////////////////////////////
+
+        // CONSTRUCTORS AND FINALISER //////////////////////////////////////////
+
+        public Item(string name, string pathToTexture, string onCollectionText = "")
+            : base(name, pathToTexture)
         {
+            // apply default onCollection text if left blank
+            if (onCollectionText == string.Empty) onCollectionText = $"You picked up a '{name}'";
         }
+
+        ~Item() => Console.WriteLine("Item object deconstructed!");
+
+        // METHODS /////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// called when the protagonist picks up an instance of this item
+        /// </summary>
+        public virtual void OnCollection()
+        {
+
+        }
+
     }
 }
