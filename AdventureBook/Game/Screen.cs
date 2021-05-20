d22a1ad75d3719e7921ec8ec59dc1bdc019b9b1c
aardhynlavender <laveat1@student.op.ac.nz>
2021-05-20 17:42:17 +1200

    code formating; minor changes

--- AdventureBook/Game/Screen.cs
+++ AdventureBook/Game/Screen.cs
@@ -6,27 +6,48 @@ namespace AdventureBook.Game
 {
     static class Screen
     {
+        // static members
+
         private static int width, height;
         private static char[][] screen;
 
+
+        /// <summary>
+        /// sets the screen size and buffer to the console specifications
+        /// </summary>
         public static void SetToConsole()
         {
+            // update the size
             width   = Console.WindowWidth;
             height  = Console.WindowHeight;
 
-            screen = new char[width][];
-            for (int x = 0; x < width; x++) for (int y = 0; y < height; y++) screen[x] = new char[width];
+            // create a new screen array
+            char[][] newScreen = new char[width][];
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++) newScreen[x] = new char[width];
+
+            // set the screen to the new screen
+            screen = newScreen;
         }
 
+
+        /// <summary>
+        /// sets the screen to ' ' characters
+        /// </summary>
         public static void Clear() {
-            for (int x = 0; x < width; x++) for (int y = 0; y < height; y++) screen[x][y] = ' ';
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++) screen[x][y] = ' ';
         }
+
+
+        // custom getters
 
         public static int GetWidth()    => width;
         public static int GetHeight()   => height;
         public static int GetCenterX()  => width / 2;
         public static int GetCenterY()  => height / 2;
 
+        // calculations
 
         public static int CenterSpriteX(Sprite sprite) => GetCenterX() - sprite.GetWidth() / 2;
         public static int CenterSpriteY(Sprite sprite) => GetCenterY() - sprite.GetHeight() / 2;
