facf7c839f21dfd28a2aa068c5e4436428dc8770
TomMc608 <tommc9010@gmail.com>
2021-05-23 10:55:12 +1200

    deleted my old work and starting again

---
+++ Tom-Horror/Tom-Horror/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tom_Horror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
