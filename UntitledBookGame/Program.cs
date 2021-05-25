d2cfd6411cde73ea398ea92288a43fad185b37cc
Aardhyn Lavender (1000096681) <LAVEAT1@student.op.ac.nz>
2021-05-26 11:01:29 +1200

    added Main project initalize

--- UntitledBookGame/Program.cs
+++ UntitledBookGame/Program.cs
@@ -12,18 +12,10 @@ namespace UntitledBookGame
                 RunCatGame, RunHorrorGame, RunMurderMysteryGame, RunPrisonEscapeGame, RunAdventureGame
             };
 
-            // book selection loop...
+            // Basic book selection loop
 
-            int selectedBook = 0;
-
-
-
-
-
-
-
-            // run the selected book
-            Books[selectedBook]();
+            Console.WriteLine("Please select a book: ");
+            Books[int.Parse(Console.ReadLine())]();
         }
     }
 }
