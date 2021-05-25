23835c40e5fc17275a366c2098ccac0ed51daed5
Jake Clearwater <jakeclearwater7@gmail.com>
2021-05-25 23:57:44 +1200

    finished first room and added menu to navigate rooms

--- Jake_studio_game/Program.cs
+++ Jake_studio_game/Program.cs
@@ -20,7 +20,7 @@ namespace Jake_studio_game
             Console.WriteLine("     |                                                  |");
             Console.WriteLine("     |                     Book II                      |");
             Console.WriteLine("     |               Press enter to begin               |");
-            Console.WriteLine("     |                                                  |");
+            Console.WriteLine("     |            please play in full screen            |");
             Console.WriteLine("     |                                                  |");
             Console.WriteLine("     |                                                  |");
             Console.WriteLine("     |                       |\\                         |");
@@ -64,12 +64,13 @@ namespace Jake_studio_game
                 Console.Clear();
                 introduction(ref name);
 
-
+                Console.WriteLine(" You are in the living room");
+                Console.ReadLine();
 
                 do
                 {
                     Console.WriteLine(" < Who would you like to speak to?");
-                    Console.WriteLine(" [1] Caitlyn [2] Hugo [3] Molly [4] Tom [5] Mr Penniworth [6] Mary [7] Leave room");
+                    Console.WriteLine(" [1] Caitlyn [2] Hugo [3] Molly [4] Tom [5] Mary [6] Leave room");
                     Console.Write(" > ");
                     selection = Console.ReadLine();
                     leave = false;
@@ -109,6 +110,7 @@ namespace Jake_studio_game
                                         break;
                                     case "3":
                                         caitlyn_q_3(ref name);
+                                        evidence_caitlyn = true;
                                         Console.Clear();
                                         break;
                                     case "4":
@@ -154,7 +156,7 @@ namespace Jake_studio_game
                             do
                             {
                                 Console.WriteLine(" < What would you like to do?");
-                                Console.WriteLine(" [1] ask what do you know about what happened? [2] ask if she killed him [3] ask who she thinks killed him [4] leave");
+                                Console.WriteLine(" [1] ask what do you know about what happened? [2] ask if he killed him [3] ask who he thinks killed him [4] leave");
                                 Console.Write(" > ");
                                 selection = Console.ReadLine();
                                 Console.Clear();
@@ -170,6 +172,7 @@ namespace Jake_studio_game
                                         break;
                                     case "3":
                                         hugo_q_3(ref name);
+                                        evidence_hugo = true;
                                         Console.Clear();
                                         break;                                    
                                     case "4":
@@ -224,6 +227,7 @@ namespace Jake_studio_game
                                     case "2":
                                         molly_q_2(ref name);
                                         Console.Clear();
+                                        evidence_molly = true;
                                         break;
                                     case "3":
                                         molly_q_3(ref name);
@@ -258,21 +262,130 @@ namespace Jake_studio_game
                                     case "4":
                                     case "Tom":
                                     case "tom":
+                            Console.Clear();
+                            Console.WriteLine($" [{name}] Excuse me, Tom?");
+                            Console.ReadLine();
+                            Console.WriteLine(" [Tom Thompson] Yes, What do you want?");
+                            Console.ReadLine();
+                            Console.WriteLine($" [{name}] Would you mind I asked you a few questions to get a grip of what happened here?");
+                            Console.ReadLine();
+                            Console.WriteLine(" [Tom Thompson] if you must");
+                            Console.ReadLine();
+                            Console.Clear();
 
+                            do
+                            {
+                                Console.WriteLine(" < What would you like to do?");
+                                Console.WriteLine(" [1] ask what do you know about what happened? [2] comfort him [3] ask if he killed him [4] ask who he thinks killed him [5] leave");
+                                Console.Write(" > ");
+                                selection = Console.ReadLine();
+                                Console.Clear();
+                                switch (selection)
+                                {
+                                    case "1":
+                                        tom_q_1(ref name);
+                                        Console.Clear();
+                                        evidence_tom = true;
+                                        break;
+                                    case "2":
+                                        tom_q_2(ref name);
+                                        Console.Clear();
+                                        break;
+                                    case "3":
+                                        tom_q_3(ref name);
+                                        Console.Clear();
+                                        break;
+                                    case "4":
+                                        tom_q_4(ref name);
+                                        Console.Clear();
                                         break;
                                     case "5":
-                                    case "Mr Penniworth":
-                                    case "mr penniworth":
-                                    case "Mr penniworth":
-                                    case "mr Penniworth":
-
+                                        Console.WriteLine($" [{name}] Thank you, that should do");
+                                        Console.ReadLine();
+                                        Console.Clear();
+                                        leave = true;
                                         break;
-                                    case "6":
+                                    default:
+                                        invalidOperator = true;
+                                        break;
+                                }
+                                if (invalidOperator == true)
+                                {
+                                    Console.WriteLine(" [Console] < Invalid input");
+
+                                    invalidOperator = false;
+                                    Console.ReadLine();
+                                    Console.Clear();
+                                }
+
+                            } while (leave == false);
+                            break;
+                                  
+
+                                  
+                                    case "5":
                                     case "Mary":
                                     case "mary":
+                            Console.Clear();
+                            Console.WriteLine($" [{name}] Excuse me, Mary?");
+                            Console.ReadLine();
+                            Console.WriteLine(" [Mary Thompson] Yes, How can i help?");
+                            Console.ReadLine();
+                            Console.WriteLine($" [{name}] Would you mind I asked you a few questions to get a grip of what happened here?");
+                            Console.ReadLine();
+                            Console.WriteLine(" [Mary Thompson] Of course");
+                            Console.ReadLine();
+                            Console.Clear();
 
+                            do
+                            {
+                                Console.WriteLine(" < What would you like to do?");
+                                Console.WriteLine(" [1] ask what do you know about what happened? [2] comfort her [3] ask if she killed him [4] ask who she thinks killed him [5] leave");
+                                Console.Write(" > ");
+                                selection = Console.ReadLine();
+                                Console.Clear();
+                                switch (selection)
+                                {
+                                    case "1":
+                                        mary_q_1(ref name);
+                                        Console.Clear();
+                                        evidence_mary = true;
                                         break;
-                                    case "7":
+                                    case "2":
+                                        mary_q_2(ref name);
+                                        Console.Clear();
+                                        break;
+                                    case "3":
+                                        mary_q_3(ref name);
+                                        Console.Clear();
+                                        break;
+                                    case "4":
+                                        mary_q_4(ref name);
+                                        Console.Clear();
+                                        break;
+                                    case "5":
+                                        Console.WriteLine($" [{name}] Thank you, that should do");
+                                        Console.ReadLine();
+                                        Console.Clear();
+                                        leave = true;
+                                        break;
+                                    default:
+                                        invalidOperator = true;
+                                        break;
+                                }
+                                if (invalidOperator == true)
+                                {
+                                    Console.WriteLine(" [Console] < Invalid input");
+
+                                    invalidOperator = false;
+                                    Console.ReadLine();
+                                    Console.Clear();
+                                }
+
+                            } while (leave == false);
+
+                            break;
+                                    case "6":
                                         Console.WriteLine(" You have left the room");
                                         Console.ReadLine();
                                         leaveroom = true;
@@ -292,6 +405,28 @@ namespace Jake_studio_game
                                     Console.Clear();
                                 }
                      } while (leaveroom == false);
+
+                Console.WriteLine("");
+                Console.WriteLine("      __________________________________________________");
+                Console.WriteLine("     |                                                  |");
+                Console.WriteLine("     |                                                  |");
+                Console.WriteLine("     |            What would you like to do?            |");
+                Console.WriteLine("     |                                                  |");
+                Console.WriteLine("     |                                                  |");
+                Console.WriteLine("     |    [1] Go Outside       [2] Go to Living room    |");
+                Console.WriteLine("     |    [3]Go Upstairs       [4] Go To Dining Room    |");
+                Console.WriteLine("     |    [5] Check evidence                            |");
+                Console.WriteLine("     |                                                  |");
+                Console.WriteLine("     |                       |\\                         |");
+                Console.WriteLine("     |                       | \\                        |");
+                Console.WriteLine("     |                       | |                        |");
+                Console.WriteLine("     |                       | |                        |");
+                Console.WriteLine("     |                       ||                         |");
+                Console.WriteLine("     |                       ||                         |");
+                Console.WriteLine("     |__________________________________________________|");
+
+                Console.Write(" > ");
+                Console.ReadLine();
 
 
             }
@@ -408,6 +543,79 @@ namespace Jake_studio_game
             Console.ReadLine();
 
         }
+        public static void tom_q_1(ref string name)
+        {
+            Console.WriteLine($" [{name}] What do you know about what happened?");
+            Console.ReadLine();
+            Console.WriteLine(" [Tom Thompson] Nothing except that hes finally dropped dead");
+            Console.ReadLine();
+            Console.WriteLine($" [{name}] Were you father not on good terms?");
+            Console.ReadLine();
+            Console.WriteLine(" [Tom Thompson] No, I was his only son and a disapointment so I wasn't given much time of day...");
+            Console.ReadLine();
+
+        }
+        public static void tom_q_2(ref string name)
+        {
+            Console.WriteLine($" [{name}] Im sorry about your loss... I won't stop until I found out who did this. You have my word");
+            Console.ReadLine();
+            Console.WriteLine(" [Tom Thompson] Thank you, the faster this is over with the faster I can get out of this awful house");
+            Console.ReadLine();
+
+        }
+        public static void tom_q_3(ref string name)
+        {
+            Console.WriteLine($" [{name}] I'll get right to the chase... Did you kill your father?");
+            Console.ReadLine();
+            Console.WriteLine(" [Tom Thompson] Wouldn't it be great if your job was that easy....");
+            Console.WriteLine(" I was kidding, No I didnt kill him...");
+            Console.ReadLine();
+        }
+        public static void tom_q_4(ref string name)
+        {
+            Console.WriteLine($" [{name}] Who do you think killed him?");
+            Console.ReadLine();
+            Console.WriteLine(" [Tom Thompson] No idea. your the detective aren't you?");            
+            Console.ReadLine();
+
+        }
+        public static void mary_q_1(ref string name)
+        {
+            Console.WriteLine($" [{name}] What do you know about what happened?");
+            Console.ReadLine();
+            Console.WriteLine(" [Mary Thompson] Well I found him in his studio with pills all over the desk and whiskey everywhere, but I know my Husband he would never");
+            Console.ReadLine();
+            Console.WriteLine($" [{name}] Were you happy with him?");
+            Console.ReadLine();
+            Console.WriteLine(" [Mary Thompson] Well... of course. Well actually no, he always cheated on me with different women every week... I would look the other way to keep him happy");
+            Console.ReadLine();
+
+        }
+        public static void mary_q_2(ref string name)
+        {
+            Console.WriteLine($" [{name}] Im sorry about your loss... I won't stop until I found out who did this. You have my word");
+            Console.ReadLine();
+            Console.WriteLine(" [Mary Thompson] Thank you, it would mean alot to us...");
+            Console.ReadLine();
+
+        }
+        public static void mary_q_3(ref string name)
+        {
+            Console.WriteLine($" [{name}] Did you kill your Husband?");
+            Console.ReadLine();
+            Console.WriteLine(" [Mary Thompson] No of course not! how dare you I loved my husband");
+            Console.ReadLine();
+        }
+        public static void mary_q_4(ref string name)
+        {
+            Console.WriteLine($" [{name}] Who do you think killed him?");
+            Console.ReadLine();
+            Console.WriteLine(" [Mary Thompson] I would hate to blame any of my kids but Tom and Molly hated him the most...");
+            
+            Console.ReadLine();
+
+        }
+       
 
 
 
