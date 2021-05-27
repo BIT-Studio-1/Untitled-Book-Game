8dc76d5609fab7dfb08b3229ac252d36e8df4703
Aardhyn Lavender (1000096681) <LAVEAT1@student.op.ac.nz>
2021-05-27 12:22:08 +1200

    added assets

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Foo(5);
+        }
+
+        static void Foo(int n)
+        {
+            n *= 2;
+            Foo(n);
+        }
+    }
+}
