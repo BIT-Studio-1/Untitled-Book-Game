6cfc7fc939692b87bf95290dc7d3e1d68a892bc4
schekt1 <schekt1@student.op.ac.nz>
2021-08-05 15:14:20 +1200

    code condensed and bugs fixed

--- UntitledBookGame/PrisonEscapeGame.cs
+++ UntitledBookGame/PrisonEscapeGame.cs
@@ -124,6 +124,9 @@ namespace UntitledBookGame
                             inventory[1] = "sock";
                             Console.WriteLine("");
                             Console.WriteLine(inventory[0] + " and a " + inventory[1] + " have been added to your inventory!");
+                            Console.ReadLine();
+                            FirstRoomPrisonCellStage2();
+
                         }
                         else
                         {
@@ -152,6 +155,7 @@ namespace UntitledBookGame
                     default:
                         Console.Clear();
                         Console.WriteLine("Invalid Input, enter directional keys 'WASD' to move");
+                        FirstRoomPrisonCell();
                         break;
 
                 }
@@ -216,147 +220,13 @@ namespace UntitledBookGame
                     default:
                         Console.Clear();
                         Console.WriteLine("Nowhere to go");
-                        CellItemsRemoved();
+                        FirstRoomPrisonCellStage2();
                         break;
 
                 }
             } while ((temp == "w") || (temp == "d") || (temp == "a") || (temp == "i"));
 
             Console.ReadLine();
-        }
-
-        static void PrisonCell() // Room 1 prison cell
-        {
-
-            string[] inventory = new string[3];
-            inventory[1] = "Screwdriver";
-            inventory[2] = "Sock";
-            string temp, takeItems;
-
-
-            do
-            {
-                Console.WriteLine("");
-                Console.WriteLine("Where to next?");
-                temp = Console.ReadLine();
-
-
-                switch (temp)
-                {
-                    case "a":
-                        Console.Clear();
-                        Console.WriteLine("Nothing to salavge from the toilet...gross");
-                        Console.WriteLine("");
-                        break;
-
-                    case "d":
-                        Console.Clear();
-                        Console.WriteLine("You find a screwdriver and a sock laying on the desk");
-                        Console.WriteLine("Press 'I' to take items");
-                        Console.WriteLine("");
-                        takeItems = Console.ReadLine();
-                        if (takeItems == "i")
-                        {
-                            inventory[0] = "screwdriver";
-                            inventory[1] = "sock";
-                            Console.WriteLine("");
-                            Console.WriteLine(inventory[0] + " and a " + inventory[1] + " have been added to your inventory!");
-                        }
-                        else
-                        {
-                            PrisonCell();
-                        }
-                        break;
-
-                    case "s":
-                        Console.Clear();
-                        Console.WriteLine("You stand on top of the bed, that vent looks to be in reach now");
-                        Console.WriteLine("You will need to pry it open with something");
-                        Console.WriteLine("");
-                        break;
-
-                    case "w":
-                        Console.Clear();
-                        Console.WriteLine("Just a wall, nothing special here");
-                        Console.WriteLine("");
-                        break;
-
-                    case "i":
-                        Console.Clear();
-                        Console.WriteLine("Nothing in inventory");
-                        break;
-
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("Nowhere to go");
-                        break;
-
-                }
-            } while ((temp == "s") || (temp == "w") || (temp == "i") || (temp == "a"));
-        }
-
-
-        static void CellItemsRemoved() //Items added to inventory executes CellItemsRemoved method
-        {
-
-            do
-            {
-                Console.WriteLine("");
-                Console.WriteLine("Where to next?");
-                temp = Console.ReadLine();
-
-
-                switch (temp)
-                {
-                    case "w":
-                        Console.Clear();
-                        Console.WriteLine("Desk top is empty");
-                        break;
-
-                    case "d":
-                        Console.Clear();
-                        Console.WriteLine("Not much use being behind these prison bars...I gotta find a way out");
-                        break;
-
-                    case "a":
-                        Console.Clear();
-                        Console.WriteLine("Nothing to salavge from the toilet...gross");
-                        break;
-
-                    case "s":
-                        Console.Clear();
-                        Console.WriteLine("You stand on top of the bed, that vent looks to be in reach now");
-                        Console.WriteLine("You need to pry it open with something");
-                        Console.WriteLine("You can equip that screwdriver you found to pry it open");
-                        Console.WriteLine("Press 'o' to pry vent open with screwdriver");
-                        pryOpen = Console.ReadLine();
-
-
-                        if (pryOpen == "o")
-                        {
-                            Console.WriteLine("");
-                            Console.WriteLine("Open seasame!");
-                            Console.WriteLine("New location unlocked! you can now access the ceiling vent");
-                        }
-                        else
-                        {
-                            CellItemsRemoved();
-                        }
-                        break;
-
-                    case "i":
-                        Console.Clear();
-                        Console.WriteLine("You currently have a" + inventory[1] + " and a " + inventory[2]);
-                        break;
-
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("Nowhere to go");
-                        CellItemsRemoved();
-                        break;
-
-                }
-            } while ((temp == "s") || (temp == "w") || (temp == "i") || (temp == "a"));
         }
     }
 }
